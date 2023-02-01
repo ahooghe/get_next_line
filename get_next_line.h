318765c95ca09204517d0058116dd9fbd4dc3326
Anthony Hooghe <ahooghe@a2-r2-p8.s19.be>
2023-02-01 10:50:04 +0100

    Files @ Campus

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahooghe <ahooghe@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 12:29:34 by ahooghe           #+#    #+#             */
+/*   Updated: 2023/01/25 12:42:07 by ahooghe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+//functions
+
+#endif
